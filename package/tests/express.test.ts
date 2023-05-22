d2d1328dd1c7ea97235a52c45216cf9608dd752f
shawshankkumar <shashankkumarthakur@gmail.com>
2023-05-22 22:15:59 +0530

    chore: Add tests

---
+++ package/tests/express.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { app } from "./express";
+
+describe("Test the correct routes", () => {
+  test("It should return 200", async () => {
+    const response = await request(app).get("/i-am-a-200");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should return 500", async () => {
+    const response = await request(app).post("/i-am-a-500");
+    expect(response.statusCode).toBe(500);
+  });
+  test("It should return 404", async () => {
+    const response = await request(app).delete("/i-am-john-cena");
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+describe("Test the broken links", () => {
+  test("It should return 500", async () => {
+    const response = await request(app).get("/i-am-a-709");
+    expect(response.statusCode).toBe(500);
+  });
+  test("It should return 200", async () => {
+    const response = await request(app).post("/i-am-a-string");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should return 500", async () => {
+    const response = await request(app).delete("/i-am-an-object");
+    expect(response.statusCode).toBe(500);
+  });
+});
---
+++ package/tests/express.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, NextFunction } from "express";
+import expressStatusValidate from "../src/index";
+
+const app = express();
+
+app.use(expressStatusValidate());
+
+app.get("/i-am-a-200", (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(200);
+});
+
+app.post("/i-am-a-500", (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(500);
+});
+
+app.delete(
+  "/i-am-john-cena",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus(404);
+  }
+);
+
+app.get("/i-am-a-709", (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(709);
+});
+
+app.post(
+  "/i-am-a-string",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus("200okay" as any);
+  }
+);
+
+app.delete(
+  "/i-am-an-object",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus({ name: "Shashank" } as any);
+  }
+);
+
+export { app };
