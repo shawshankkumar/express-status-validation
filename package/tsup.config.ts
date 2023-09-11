e8f598c6981b7bf11c694fdaabff755da17eeb70
shawshankkumar <shashankkumarthakur@gmail.com>
2023-09-11 19:41:57 +0530

    chore: minor changes

--- package/tsup.config.ts
+++ package/tsup.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  target: ["node18", "node16"],
   shims: false,
-  // dts: true, 
+  dts: true, 
   clean: true,
   treeshake: true,
   sourcemap: false,
